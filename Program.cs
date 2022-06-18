69af2ae4eec8c88cf2c5395c04c76ea940fd6dce
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2022-06-18 14:03:35 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace book_stats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
